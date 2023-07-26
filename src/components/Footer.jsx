297970931b274296c5dbbb547242f9cf8a9fd00b
Hale Bradley <hbradley787@gmail.com>
2023-07-25 20:46:49 -0500

    Fix bugs and warnings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaDiscord, FaGithubSquare, FaInstagram, FaTwitter } from 'react-icons/fa'
+import { FaGithubSquare, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -8,9 +8,9 @@ const Footer = () => {
             <h1 className='w-full text-3xl font-bold text-[#00df9a]'>hb</h1>
             <p className='py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa suscipit autem iure nulla omnis eaque nesciunt soluta eligendi magni illum!</p>
             <div className='flex justify-between md:w-[75%] my-6'>
-                <a href='https://github.com/HaleBradley' target="_blank"><FaGithubSquare size={30} /></a>
-                <a href='https://twitter.com/HaleTheDev' target="_blank"><FaTwitter size={30} /></a>
-                <a href='https://www.instagram.com/hale_bradley/' target="_blank"><FaInstagram size={30} /></a>
+                <a href='https://github.com/HaleBradley' target="_blank" rel="noreferrer"><FaGithubSquare size={30} /></a>
+                <a href='https://twitter.com/HaleTheDev' target="_blank" rel="noreferrer"><FaTwitter size={30} /></a>
+                <a href='https://www.instagram.com/hale_bradley/' target="_blank" rel="noreferrer"><FaInstagram size={30} /></a>
             </div>
         </div>
         
